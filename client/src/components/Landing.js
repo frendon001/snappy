e0933912f67e6a94e800cc8e314ca84784816dd2
Fausto Rendon <faustor001@gmail.com>
2018-08-05 13:16:33 -0700

    Create simple component for landing page

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Snappy!</h1>Collect feedack from your users
+    </div>
+  );
+};
+
+export default Landing;
