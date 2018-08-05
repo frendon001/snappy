1b3547ecc4de014e2501b79d7c806381ea50ba73
Fausto Rendon <faustor001@gmail.com>
2018-08-05 15:06:02 -0700

    Add Payments component to Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderContent() {
@@ -14,16 +15,18 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
-          <li>
+        return [
+          <li key="payment">
+            <Payments />
+          </li>,
+          <li key="logout">
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
 
   render() {
-    console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
