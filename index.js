0bb29485356e32e9544376e3c2dab281b9550098
Fausto Rendon <faustor001@gmail.com>
2018-08-06 22:11:39 -0700

    Add routing for front end in production

--- index.js
+++ index.js
@@ -28,5 +28,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //Express to serve up production assets
+  app.use(express.static('client/build'));
+  //Expess will serve up index.html file
+  //if route is not recognized
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
