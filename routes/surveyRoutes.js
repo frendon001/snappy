b872518ea3b3f39b4b79029ee96f168cd313ff82
Fausto Rendon <faustor001@gmail.com>
2018-08-11 00:31:38 -0700

    Add sendgrid mailer to survey route - test sucessfull

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -17,5 +19,10 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    // Create new email mailer for sendgrid
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
+    res.send('Good');
   });
 };
