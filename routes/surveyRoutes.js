6e26945771db88c87f6252c429c2e8524661837b
Fausto Rendon <faustor001@gmail.com>
2018-08-14 23:59:41 -0700

    Add querry to survey route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -19,7 +19,7 @@ module.exports = app => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const surveyPath = new Path('/api/surveys/:surveyId/:choice');
     //obtain valid survey answers
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const matchPath = surveyPath.test(new URL(url).pathname);
         if (matchPath) {
@@ -32,9 +32,22 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true }
+          }
+        ).exec();
+      })
       .value();
 
-    console.log(events);
     res.send({});
   });
 
