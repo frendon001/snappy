4b35445671904b639bb58f2d70e938993a4f4e37
Fausto Rendon <faustor001@gmail.com>
2018-08-16 22:49:47 -0700

    Add Survey List rendering to app

---
+++ client/src/components/surveys/SurveyList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurvey() {
+    return this.props.surveys.reverse().map(survey => {
+      return (
+        <div className="card blue-grey lighten-5" key={survey._id}>
+          <div className="card-content ">
+            <span className="card-title">{survey.title}</span>
+
+            <p>{survey.body}</p>
+            <p className="right">
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a className="orange-text text-accent-3">Yes: {survey.yes}</a>
+            <a className="orange-text text-accent-3">No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="col s12 m12">{this.renderSurvey()} </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchSurveys }
+)(SurveyList);
