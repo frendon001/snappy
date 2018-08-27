91556514d9966d9d4c48e6895e866c825daa6166
Fausto Rendon <faustor001@gmail.com>
2018-08-26 22:28:19 -0700

    Add delete survey option to surveys reducer

--- client/src/reducers/surveysReducer.js
+++ client/src/reducers/surveysReducer.js
@@ -1,9 +1,18 @@
-import { FETCH_SURVEYS } from '../actions/types';
+import _ from 'lodash';
+import { FETCH_SURVEYS, DELETE_SURVEY } from '../actions/types';
 
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_SURVEYS:
       return action.payload;
+    case DELETE_SURVEY:
+      console.log(state);
+      return _.chain(state)
+        .remove(function(survey) {
+          return survey._id !== action.payload;
+        })
+        .reverse()
+        .value();
     default:
       return state;
   }
